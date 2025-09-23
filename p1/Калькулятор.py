8932451ba6970babe10c94411e4019b343a81cdf
KindSugar <valeriya-lerusik@mail.ru>
2025-09-23 11:50:49 +1000

    Update Калькулятор.py

--- p1/Калькулятор.py
+++ p1/Калькулятор.py
@@ -1,1 +1,37 @@
+#Калькулятор
+
+while True:
+    a = input("Первое число: ")
+    b = input("Второе число: ")
+    c = input ("Введите знак:")
+
+    if c == "+":
+        try:
+            print(float(a) + float(b))
+        except:
+            print("Error!")
+    elif c == "-":
+        try:
+            print(float(a) - float(b))
+        except:
+            print("Error!")
+    elif c == "*":
+        try:
+            print(float(a) * float(b))
+        except:
+            print("Error!")
+    elif c == "**":
+        try:
+            print(float(a) ** float(b))
+        except:
+            print("Error!")
+    elif c == "/":
+        try:
+            print(float(a) / float(b))
+        except:
+            print("Error!")
+
+
+
+
 
